ae3a99af4ee858508f6eebacf9ca5c02611cc4f1
Muhammad Hasnain <tryfrelancer23@gmail.com>
2025-07-24 15:25:28 +0500

    updated eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -16,6 +16,7 @@ const eslintConfig = [
       "react/no-unescaped-entities": "off",
       "@typescript-eslint/no-unused-vars": "off",
       "react-hooks/exhaustive-deps": "off",
+      "@typescript-eslint/no-explicit-any": "off",
     },
   },
 ];
